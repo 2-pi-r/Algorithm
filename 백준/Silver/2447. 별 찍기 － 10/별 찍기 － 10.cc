86d4164dc597c6277d0e8a0cd4a7dcc5dc22e1c0
2-pi-r <87740183+2-pi-r@users.noreply.github.com>
2022-05-17 19:18:44 +0900

    Update 별 찍기 － 10.cc

--- 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -23,16 +23,18 @@ void star(int x, int y, int n, char** arr)
 int main() {
 	int N; cin >> N;
 	
-	char** arr = new char* [N]; //전역변수, 2차원 배열, 동적할당
+	char** arr = new char* [N]; //2차원 배열, 동적할당
 	for (int i = 0; i < N; i++)
 		arr[i] = new char[N];
 	
-	for (int i = 0; i < N; i++) ////전부 공백으로 초기화
+	for (int i = 0; i < N; i++) //전부 공백으로 초기화 (이 방법 말고 다른 방법은 못 찾았음.)
 		for (int j = 0; j < N; j++)
 			arr[i][j] = ' ';
 
 	star(0, 0, N, arr);
 	
+	//char배열 끝에 '\0'이 없어서, cout << arr[i] 처럼 한줄(문자열)씩 출력할 수 없음.
+	//cout << arr 하면 아예 arr(이중 포인터)에 저장된 주소가 출력됨.
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++)
 			cout << arr[i][j];
