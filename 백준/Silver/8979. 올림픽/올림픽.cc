8512027b237c7785657bb517f6dcbe34a63cb7da
2-pi-r <87740183+2-pi-r@users.noreply.github.com>
2022-07-12 23:58:55 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -1,39 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-typedef pair<int, long long> P;
-
-int N, K;
-
-bool compare(P x, P y) {
-	if (x.second == y.second)
-		return x.first == K;
-	return x.second > y.second;
-}
-
-int findIndex(vector<P> &v, int country) {
-	for (int i = 0; i < v.size(); i++) {
-		if (v[i].first == country)
-			return i;
-	}
-}
-
 int main() {
-	cin >> N >> K; //국가의 수, 등수를 알고 싶은 국가
-	vector<P> medal(N + 1);
+	int N, K; cin >> N >> K; //국가의 수, 등수를 알고 싶은 국가
+	vector<long long> medal(N + 1);
 
 	int country, gold, silver, bronze;
     long long M = 1000000; //전체 메달 수의 총합은 1,000,000 이하
 	while (N--) {
 		cin >> country >> gold >> silver >> bronze;
-		medal.push_back( { country, M*M*gold + M*silver + 1*bronze} );
+		medal[country] = M*M*gold + M*silver + bronze;
 	}
 
-	sort(medal.begin(), medal.end(), compare); //내림차순 + 같은 점수면 K나라가 우선.
-
-	cout << findIndex(medal, K) + 1;
+	int result = 0;
+	for (int i = 1; i < medal.size(); i++) {
+		if (medal[i] > medal[K])
+			result++;
+	}
+	cout << result + 1;
 
 	return 0;
 }
