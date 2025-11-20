1396201b509a916041da74168a0084c1352b06c5
2-pi-r <87740183+2-pi-r@users.noreply.github.com>
2025-11-21 01:20:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// input
+	int n; cin >> n;
+
+	// solution
+	vector<int> dp(n+1);
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+	}
+	
+	cout << dp[n];
+
+	return 0;
+}
